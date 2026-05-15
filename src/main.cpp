be539a50b8af896096a539f0839f918d751bff9a
Swaroop <swaroopcharan1712@gmail.com>
2026-05-15 15:47:47 +0530

    added sharpen

--- src/main.cpp
+++ src/main.cpp
@@ -74,6 +74,20 @@ int main() {
         free(result);
     }
     
+    else if(choice == 4) {
+        float boxBlur[3][3] = {
+            {0, -1, 0},
+            {-1, 5, -1},
+            {0, -1, 0}
+        };
+
+        applyKernel(img, result, width, height, channels, boxBlur);
+        stbi_write_png("C:/Users/Swaroop/dev/projects/image filter engine/samples/output/sharpen.png", width, height, channels, result, width * channels);
+
+        stbi_image_free(img);
+        free(result);
+    }
+    
 
     return 0;
 }
